29897115c94d3af3ccc7321e4caa63285244945f
Bernie Zhong <berniezhong@gmail.com>
2021-12-11 15:48:04 -0500

    Start/top screen completed

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const WORLD_HEIGHT = 30;
 const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElement = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
 
 setPixelToWorldScale();
 
@@ -14,6 +16,7 @@ document.addEventListener("keydown", handleStart, { once: true });
 
 let lastTime;
 let speedScale;
+let score;
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -24,6 +27,7 @@ function update(time) {
 
   updateGround(delta, speedScale);
   updateSpeedScale(delta);
+  updateScore(delta);
 
   lastTime = time;
   window.requestAnimationFrame(update);
@@ -32,10 +36,20 @@ function update(time) {
 function updateSpeedScale(delta) {
   speedScale += delta * SPEED_SCALE_INCREASE;
 }
+
+function updateScore(delta) {
+  // for every second we get 10 points
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+
 function handleStart() {
   lastTime = null;
   speedScale = 1;
+  score = 0;
   setupGround();
+  // this targets our startscreen element and hides it
+  startScreenElem.classList.add("hide");
   // this will call our update function when the content on our screen changes
   window.requestAnimationFrame(update);
 }
